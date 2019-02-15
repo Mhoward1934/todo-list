12bd0a93a430f3ed9c4c16a18ee481bc22538500
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-02-15 11:39:24 -0600

    add db connection

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const app = express();
 
+const dbconnect = process.env.MONGODB_URI || 'mongodb://localhost/todolist';
+const db = mongoose.connect(dbconnect);
+
 const PORT = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
